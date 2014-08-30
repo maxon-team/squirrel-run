16791f5083daaa3166d916df4b30b2d44327a8c0
Zhi <yfwz100@yeah.net>
2014-08-30 09:59:07 +0800

    Added touch listener and simple gesture recognizer.

--- src/layer/game-layer.js
+++ src/layer/game-layer.js
@@ -6,6 +6,9 @@ var GameLayer = cc.Layer.extend({
 	// the hero sprite.
 	sprite: null,
 	spriteSheet: null,
+	
+	// the recognizer.
+	recognizer: null,
 
 	// runner speed.
 	runnerSpeed: 35,
@@ -14,11 +17,19 @@ var GameLayer = cc.Layer.extend({
 		this._super();
 		this.space = space;
 		this.initSprite();
+		cc.eventManager.addListener({
+			event: cc.EventListener.TOUCH_ONE_BY_ONE,
+			swallowTouches: true,
+			onTouchBegan: this.onTouchBegan,
+			onTouchMoved: this.onTouchMoved,
+			onTouchEnded: this.onTouchEnded
+		}, this);
+		this.recognizer = new SimpleTouchRecognizer();
 		this.scheduleUpdate();
 	},
 
 	initSprite: function() {
-		var winSize = cc.director.getWinSize();//get windows size
+		var winSize = cc.director.getWinSize();
 		var centerPos = cc.p(winSize.width / 2, winSize.height / 2);
 		
 		cc.spriteFrameCache.addSpriteFrames(res.squirrel.plist);
@@ -32,7 +43,7 @@ var GameLayer = cc.Layer.extend({
 			animFrames.push(frame);
 		}
 		var animation = new cc.Animation(animFrames, 0.15);
-		this.runningAction = cc.RepeatForever.create(new cc.Animate(animation));
+		this.runningAction = new cc.RepeatForever(new cc.Animate(animation));
 
 		this.sprite = new cc.PhysicsSprite("#run_01.png");
 
@@ -45,19 +56,44 @@ var GameLayer = cc.Layer.extend({
 
 		var shape = new cp.BoxShape(body, contentSize.width - 14, contentSize.height);
 		this.space.addShape(shape);
-		
+
 		this.sprite.setBody(body);
 
 		this.sprite.runAction(this.runningAction);
 		this.spriteSheet.addChild(this.sprite);
 	},
 	
+	onTouchBegan: function (touch, event) {
+		var pos = touch.getLocation();
+		event.getCurrentTarget().recognizer.beginPoint(pos.x, pos.y);
+		return true;
+	},
+
+	onTouchMoved: function (touch, event) {
+		var pos = touch.getLocation();
+		event.getCurrentTarget().recognizer.movePoint(pos.x, pos.y);
+	},
+
+	onTouchEnded: function (touch, event) {
+		var rtn = event.getCurrentTarget().recognizer.endPoint();
+		switch (rtn) {
+		case "up":
+			event.getCurrentTarget().jump();
+			break;
+		default:
+			break;
+		}
+	},
+	
+	jump: function() {
+		// TODO: jump logic.
+	},
+
 	getEyeX: function () {
 		return this.sprite.getPositionX() - this.runnerSpeed;
 	},
 
 	createPlatform : function(boardX, boardY, length) {
-
 		var platform_l = cc.Sprite.create(res.platform.left);
 		platform_l.setPosition(cc.p(boardX, boardY));
 		platform_l.attr({
@@ -79,7 +115,7 @@ var GameLayer = cc.Layer.extend({
 		}
 
 
-		var platform_r = cc.Sprite.create(res.platform.right);
+		var platform_r = new cc.Sprite(res.platform.right);
 		platform_r.attr({
 			anchorX : 0,
 			anchorY : 0
